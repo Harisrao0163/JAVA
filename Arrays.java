8adbc769d10f811e894ff57f65f52ad7123521d6
Muhammad Haris <raoh0163@gmail.com>
2025-08-04 12:24:37 +0000

    Arrays in JAVA

---
+++ Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args){ 
+        int[] numbers = new int[5];
+        numbers[0] = 10;
+        numbers[1] = 20;
+        
+        System.out.println(numbers);
+      }
+    }
