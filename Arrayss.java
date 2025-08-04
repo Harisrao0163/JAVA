436a4c4725686dd7eddff111fe64a3a125061829
Muhammad Haris <raoh0163@gmail.com>
2025-08-04 22:28:19 +0000

    Arrays in JAVA

--- Arrayss.java
+++ Arrayss.java
@@ -1,9 +1,22 @@
 import java.util.Arrays;
 
-public class Arrayss {
+   public class Arrayss
+    {
     public static void main(String[] args){ 
-        int[] numbers = {1,2,3,4,5};
+        int[] numbers = {1,2,3,6,9,8,4,5};
+        System.out.println(numbers.length);
+       Arrays.sort(numbers); // this will sortout all the numbers. 
         
         System.out.println(Arrays.toString(numbers));
-      }
+      }}
+    
+    public class Arrayss {
+    public static void main(String[] args){
+     int[][] numbers = new int[2][3];
+     numbers[0][0] = 10;
+     System.out.println(Arrays.deepToString(numbers)); // but if add one more digit then we use shortcut. see below
+      int [][] matrix = {{1,2,3,4},{5,6,7,8}};
+      System.out.println(Arrays.deepToString(matrix));
     }
+  } 
+        
