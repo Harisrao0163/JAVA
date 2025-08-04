d7db515c7d95eb50c623e9adc78c4836fe489f66
Muhammad Haris <raoh0163@gmail.com>
2025-08-04 22:42:45 +0000

    Constants in JAVA

---
+++ Constant.java
@@ -0,0 +1,5 @@
+public class Constant{
+    public static void main(String[] args){
+    final float PI = 3.14F; // now, we can't change the value of pi bcz, we used the syntax "final". and have used F with 3.14 bcz, it is in decimal.  
+    }
+}
